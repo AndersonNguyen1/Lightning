54690e662f4b771b3ce2e546c2cb0b5a754cc854
AndersonNguyen1 <anguyen135@gmail.com>
2014-08-31 12:53:04 -0700

    YAY I FINISHED

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,61 @@
+int startX = 0;
+int startY = 0;
+int endX = 0;
+int endY;
+
+int canvasSize = 500;
+
+int cloudX = 0;
+
+int x = 0;
+int y = 0;
+
 void setup()
 {
-  size(300,300);
+    size(canvasSize, canvasSize);
+    strokeWeight(3);
+    background(0);
 }
+
 void draw()
 {
-
+	
 }
+
+void drawLighting()
+{
+	while (endY < 500)
+	{
+		endX = startX + (int)(Math.random()*20 - 10);				
+		endY = startY + (int)(Math.random()*20);		
+		line (startX,startY,endX,endY);				
+		startX = endX;						
+		startY = endY;								
+	}
+}
+
 void mousePressed()
 {
-
+	stroke(random(255), random(255), random(255));
+    drawLighting();
+    drawClouds();
+    endX = 0;
+    endY = 0;
+    startX = (int)(Math.random()*canvasSize);
+    startY = 0;
 }
 
+void drawClouds()
+{
+	fill(255);
+	stroke(255);
+	while (cloudX < canvasSize) 
+	{
+		ellipse(x, y, 100, 100);
+		x+=70;
+		cloudX++;
+	}
+	cloudX = 0;
+	x = 0;
+	y = 0;
+}
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,67 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX = 0;
+int startY = 0;
+int endX = 0;
+int endY;
+
+int canvasSize = 500;
+
+int cloudX = 0;
+
+int x = 0;
+int y = 0;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
-}
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
-}
-public void mousePressed()
-{
-  redraw();
+    size(canvasSize, canvasSize);
+    strokeWeight(3);
+    background(0);
 }
 
+public void draw()
+{
+	
+}
+
+public void drawLighting()
+{
+	while (endY < 500)
+	{
+		endX = startX + (int)(Math.random()*20 - 10);				
+		endY = startY + (int)(Math.random()*20);		
+		line (startX,startY,endX,endY);				
+		startX = endX;						
+		startY = endY;								
+	}
+}
+
+public void mousePressed()
+{
+	stroke(random(255), random(255), random(255));
+    drawLighting();
+    drawClouds();
+    endX = 0;
+    endY = 0;
+    startX = (int)(Math.random()*canvasSize);
+    startY = 0;
+}
+
+public void drawClouds()
+{
+	fill(255);
+	stroke(255);
+	while (cloudX < canvasSize) 
+	{
+		ellipse(x, y, 100, 100);
+		x+=70;
+		cloudX++;
+	}
+	cloudX = 0;
+	x = 0;
+	y = 0;
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
